f512d6d8e9f236b02f0c1168c77659134801381c
Tanya Shylock <tourtanya@gmail.com>
2023-09-19 14:40:56 -0600

    add user login and sign up forms

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "house-marketplace-app-43e2b.firebaseapp.com",
+  projectId: "house-marketplace-app-43e2b",
+  storageBucket: "house-marketplace-app-43e2b.appspot.com",
+  messagingSenderId: "857538879058",
+  appId: "1:857538879058:web:9e316ba784be12323e498b",
+  measurementId: "G-Z1SCPC4MQS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const db = getFirestore();
---
+++ src/pages/Explore.js
@@ -0,0 +1,10 @@
+function Explore() {
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+    </div>
+  )
+}
+export default Explore
