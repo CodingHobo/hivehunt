b7f712d3b296858cbf4d14305fa846f0654f078e
Tanya Shylock <tourtanya@gmail.com>
2023-09-24 21:28:26 -0600

    add env variables to firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "house-marketplace-app-43e2b.firebaseapp.com",
-  projectId: "house-marketplace-app-43e2b",
-  storageBucket: "house-marketplace-app-43e2b.appspot.com",
-  messagingSenderId: "857538879058",
-  appId: "1:857538879058:web:9e316ba784be12323e498b",
-  measurementId: "G-Z1SCPC4MQS"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
